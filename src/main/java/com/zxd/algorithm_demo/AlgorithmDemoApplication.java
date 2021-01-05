b643692c831f3e56a3e1eea4dc7d93d131451f41
xuedong <1726211067@qq.com>
2021-01-05 10:35:28 +0800

    初始化

---
+++ src/main/java/com/zxd/algorithm_demo/AlgorithmDemoApplication.java
@@ -0,0 +1,13 @@
+package com.zxd.algorithm_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlgorithmDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AlgorithmDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/zxd/algorithm_demo/AlgorithmDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zxd.algorithm_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlgorithmDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
